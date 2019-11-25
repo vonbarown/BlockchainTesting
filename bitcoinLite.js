629da731461a47d5b1948d73b4e1f1b304b94e3e
vonbarown <vonielbrown@pursuit.org>
2019-11-24 19:29:45 -0500

    working oon creating the blockchain

--- bitcoinLite.js
+++ bitcoinLite.js
@@ -7,12 +7,12 @@ class Block {
         this.data = data;
         this.trans
         this.prevHash = prevHash;
-        this.thisHash = sha256(
-            this.index + this.timestamp + this.data + this.prevHash
-        );
+        this.thisHash = this.calculateHash();
         this.nonce = 0;
     }
 
+    //calculate the has
+    calculateHash = () => (sha256(this.index + this.timestamp + this.data + this.prevHash));
 
     mineBlock(difficulty) {
         while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
@@ -22,8 +22,22 @@ class Block {
 
         console.log("BLOCK MINED: " + this.hash);
     }
+
 }
 
 const createGenesisBlock = () => new Block(0, new Date(), 'Genesis Block', '0');
 
 const nextBlock = (lastBlock, data) => new Block(lastBlock.index + 1, new Date(), data, lastBlock.thisHash);
+
+class CreateBlockchain {
+    constructor() {
+        this.blockchain = [createGenesisBlock()];
+        let previousBlock = blockchain[0];
+
+        for (let i = 1; i < num; i += 1) {
+            const blockToAdd = nextBlock(previousBlock, `This is block #${i}`);
+            blockchain.push(blockToAdd);
+            previousBlock = blockToAdd;
+        }
+    }
+};
