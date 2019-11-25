4fc7b3a01df3f82d87f26f6ababd580f1cbf396c
vonbarown <vonielbrown@pursuit.org>
2019-11-24 19:04:40 -0500

    creating the block chain

--- tinyBlockchain.js
+++ tinyBlockchain.js
@@ -15,3 +15,18 @@ class Block {
 const createGenesisBlock = () => new Block(0, new Date(), 'Genesis Block', '0');
 
 const nextBlock = (lastBlock, data) => new Block(lastBlock.index + 1, new Date(), data, lastBlock.thisHash);
+
+const createBlockchain = num => {
+    const blockchain = [createGenesisBlock()];
+    let previousBlock = blockchain[0];
+
+    for (let i = 1; i < num; i += 1) {
+        const blockToAdd = nextBlock(previousBlock, `This is block #${i}`);
+        blockchain.push(blockToAdd);
+        previousBlock = blockToAdd;
+    }
+    console.log(blockchain);
+};
+
+const lengthToCreate = 20;
+createBlockchain(lengthToCreate);
